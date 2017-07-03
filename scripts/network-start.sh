15ceb6b02d6327f2fba0ace9dc63ddb4c88861d2
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-07-03 10:23:18 +0200

    Fixing the host IP address

--- scripts/network-start.sh
+++ scripts/network-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ifconfig lo1 create
-ifconfig lo1 inet 127.1.0.0 netmask 0xffff0000
+ifconfig lo1 inet 127.1.0.1 netmask 0xffff0000
 kldload -n pf
 pfctl -f /opt/carton/conf/pf.conf
 pfctl -e

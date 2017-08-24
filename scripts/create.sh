4b8826732e03d6549355a4ad49749d55927edf14
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-08-24 11:13:30 +0200

    Initial rewrite of the create script, that take care of creates directories and zfs datasets

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,37 +1,90 @@
 #!/bin/sh
 
-CARTON_ROOT=/opt/carton
+JOCKER_ZFS_ROOT=zroot/carton
+JOCKER_FS_ROOT=/opt/carton
 
-CARTON_JAILS=${CARTON_ROOT}/jails
+# derived entries
+JOCKER_ZFS_BASE=${JOCKER_ZFS_ROOT}/bases
+JOCKER_ZFS_JAIL=${JOCKER_ZFS_ROOT}/jails
+JOCKER_FS_JAIL=${JOCKER_FS_ROOT}/jails
 
-if [ -z "$1" ]; then
-	echo "Please provide a jail name"
-	exit
+# It check if the root zfs datasets are present
+is_zfs_ready()
+{
+	local _output
+	for _dataset in ${JOCKER_ZFS_ROOT} ${JOCKER_ZFS_BASE} ${JOCKER_ZFS_JAIL} ; do
+		_output="$(zfs get -H mountpoint ${_dataset} 2>/dev/null)"
+		if [ "$?" -ne 0 ]; then
+			echo ${_dataset} not existing \[ $_output \]
+			return 1 # false
+		fi
+	done
+	_output="$(zfs get -H mountpoint ${JOCKER_ZFS_JAIL} 2>/dev/null)"
+	if [ "$( echo $_output | awk '{print $3}')" != "none" ]; then
+		echo ${_dataset} has the wrong mountpoint
+		return 1 # false
+	fi
+}
+
+_is_zfs_dataset()
+{
+	local _dataset _output
+	_dataset=$1
+	if [ -z "${_dataset}" ]; then
+		return 1 # false
+	fi
+	_output="$(zfs get -H mountpoint ${_dataset} 2>/dev/null)"
+	return $?
+}
+
+_get_last_zfs_snap()
+{
+	local _dataset _output
+	_dataset=$1
+	if [ -z "$dataset" ]; then
+		return
+	fi
+	_output="$( zfs list -d 1 -H -t snapshot $_dataset | sort -r | cut -d'@' -f2 | cut -f1)"
+	echo $_output
+}
+
+# $1 the jailname
+# $2 the FreeBSD base version
+create_jail_fs()
+{
+	local _jailname _jaildir _basever _snap
+	_jailname=$1
+	_basever=$2
+	if [ -z "${_jailname}" ]; then
+		return 1 # false
+	fi
+	if ! is_zfs_ready ; then
+		return 1 # false
+	fi
+	if ! _is_zfs_dataset ${JOCKER_ZFS_JAIL}/${_jailname} ; then
+		zfs create ${JOCKER_ZFS_JAIL}/${_jailname}
+	fi
+	_jaildir=${JOCKER_FS_JAIL}/${_jailname}
+	if [ ! -d ${_jaildir} ]; then
+		mkdir -p ${_jaildir}/m
+	fi
+	if ! _is_zfs_dataset ${JOCKER_ZFS_JAIL}/${_jailname}/usr.local ; then
+		# TODO check if the specific base exists
+		if ! _is_zfs_dataset _get_last_zfs_snap ${JOCKER_ZFS_BASE}/${_basever} ; then
+			return 1 # false
+		fi
+		# looking for the last snapshot
+		_snap=$( _get_last_zfs_snap ${JOCKER_ZFS_BASE}/${_basever} )
+		if [ -z "$_snap" ]; then
+			return 1 # false
+		fi
+		zfs clone -o mountpoint=${_jaildir}/usr.local ${JOCKER_ZFS_BASE}/${_basever}/usr.local@${_snap} ${JOCKER_ZFS_JAIL}/${_jailname}/usr.local
+	fi
+	zfs clone -o mountpoint=${_jaildir}/custom ${JOCKER_ZFS_BASE}/11.1/custom@p1 ${JOCKER_ZFS_JAIL}/${_jailname}/custom
+}
+
+if ! is_zfs_ready ; then
+	echo "The zfs infra is not ready"
+else
+	echo "The zfs infra is fine"
 fi
-NEW_JAIL=$1
-shift
-if [ -d ${CARTON_JAILS}/${NEW_JAIL} ]; then
-	echo "The jail ${NEW_JAIL} already exists"
-	echo "[ remove the ${CARTON_JAILS}/${NEW_JAIL} directory ]"
-	exit
-fi
-
-if [ -z "$1" ]; then
-	echo "Please provide a FreeBSD version"
-	exit
-fi
-REL=$1
-shift
-if [ ! -d ${CARTON_ROOT}/bases/${REL} ]; then
-	echo "The release $1 doesn't exists"
-	exit
-fi
-
-
-mkdir -p ${CARTON_JAILS}/${NEW_JAIL}
-
-for subd in conf et local m var ; do
-	mkdir -p ${CARTON_JAILS}/${NEW_JAIL}/$subd
-done
-
-

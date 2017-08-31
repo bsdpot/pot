b277befc1a777d2b4a29aae401283e0ec0caa955
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-08-31 16:22:16 +0200

    making jail-stop unmount process more robust

--- scripts/jail-stop.sh
+++ scripts/jail-stop.sh
@@ -23,13 +23,40 @@ if [ -f ${CARTON_JAILS}/${JAIL}/m/etc/resolv.conf ]; then
 	rm ${CARTON_JAILS}/${JAIL}/m/etc/resolv.conf
 fi
 
+_is_mounted() {
+	local _mntpoint
+	_mntpoint=$1
+	if [ -z "$_mntpoint" ]; then
+		return 1 # false
+	fi
+	local _mounted
+	_mounted=$( mount | grep $_mntpoint | grep -v zfs | awk '{print $3}')
+	for m in "$_mounted" ; do
+		if [ "$_mntpoint" = "$m" ]; then
+			return 0 # true
+		fi
+	done
+	return 1 # false
+}
+
+_umount() {
+	local _mntpoint
+	_mntpoint=$1
+	if [ -z "$_mntpoint" ]; then
+		return 1 # false
+	fi
+	if _is_mounted $_mntpoint ; then
+		umount -f $_mntpoint
+	fi
+}
+
 umount_fs() {
 	tail -r ${CARTON_JAILS}/${JAIL}/conf/fs.conf > /tmp/fs.conf
-	umount ${CARTON_JAILS}/${JAIL}/m/tmp
-	umount ${CARTON_JAILS}/${JAIL}/m/dev
+	_umount ${CARTON_JAILS}/${JAIL}/m/tmp
+	_umount ${CARTON_JAILS}/${JAIL}/m/dev
 	while read -r line ; do
 		mount_point=$( echo $line | awk '{ print $2 }')
-		umount $mount_point
+		_umount $mount_point
 	done < /tmp/fs.conf
 }
 umount_fs

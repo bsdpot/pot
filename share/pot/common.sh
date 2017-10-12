1ad46de00a2256872d5126935df3ccfb1c691ae0
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-10-12 18:29:41 +0200

    Add _is_in_list utility function and fix _zfs_exist

--- share/pot/common.sh
+++ share/pot/common.sh
@@ -14,7 +14,7 @@ _zfs_exist()
 {
 	[ -z "$1" -o -z "$2" ] && return 1 ## return false
 	zfs list "$1" 2> /dev/null > /dev/null
-	[ $? -ne 0 ] && return $?
+	[ $? -ne 0 ] && return 1 ## false
 	local _mnt_
 	_mnt_=$(zfs get -H mountpoint $1 2> /dev/null | awk '{ print $3 }')
 	if [ "$_mnt_" != "$2" ]; then
@@ -22,6 +22,25 @@ _zfs_exist()
 	fi
 	return 0 ## true
 }
+
+# $1 the element to search
+# $2.. the list
+_is_in_list()
+{
+	local _e
+	if [ $# -lt 3 ]; then
+		return 1 # false
+	fi
+	_e="$1"
+	shift
+	for e in $@ ; do
+		if [ "$_e" = "$e" ]; then
+			return 0 # true
+		fi
+	done
+	return 1 # false
+}
+
 pot-cmd()
 {
 	local _cmd _func

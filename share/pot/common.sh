ae21dfa1f2a13a8b8d6ef132db63aaacbd0c601a
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-10-12 15:10:45 +0200

    Implementing a way to invoke subcommands in a scalable fashion

--- share/pot/common.sh
+++ share/pot/common.sh
@@ -2,11 +2,14 @@
 
 pot-cmd()
 {
-	local _cmd
+	local _cmd _func
 	_cmd=$1
-
+	shift
 	if [ ! -r "${_POT_INCLUDE}/${_cmd}.sh" ]; then
 		echo "Fatal error! $_cmd implementation not found!"
 		exit 1
 	fi
+	. ${_POT_INCLUDE}/${_cmd}.sh
+	_func=pot-${_cmd}
+	$_func $@
 }

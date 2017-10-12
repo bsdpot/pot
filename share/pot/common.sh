990db9529206382b4f88aebdc927ec2fa40d2e3b
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-10-12 15:51:26 +0200

    Adding some zfs related routines

--- share/pot/common.sh
+++ share/pot/common.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# $1 the dataset NAME
+_zfs_is_dataset()
+{
+	[ -z "$1" ] && return 1 ## return false
+	zfs list "$1" 2> /dev/null > /dev/null
+	return $?
+}
+
+# $1 the dataset NAME
+# $2 the mountpoint
+_zfs_exist()
+{
+	[ -z "$1" -o -z "$2" ] && return 1 ## return false
+	zfs list "$1" 2> /dev/null > /dev/null
+	[ $? -ne 0 ] && return $?
+	local _mnt_
+	_mnt_=$(zfs get -H mountpoint $1 2> /dev/null | awk '{ print $3 }')
+	if [ "$_mnt_" != "$2" ]; then
+		return 1 ## false
+	fi
+	return 0 ## true
+}
 pot-cmd()
 {
 	local _cmd _func

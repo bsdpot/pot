c88fe4e7f32b7ad911e97ad8d537c477e294831c
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-10-12 15:52:32 +0200

    Making the root creation process more robust

--- share/pot/init.sh
+++ share/pot/init.sh
@@ -2,11 +2,25 @@
 
 pot-init()
 {
-	# create the pot root
-	echo zfs create -o mountpoint=${POT_FS_ROOT} -o canmount=off -o compression=lz4 -o atime=off ${POT_ZFS_ROOT}
+	if ! _zfs_exist "${POT_ZFS_ROOT}" "${POT_FS_ROOT}" ; then
+		if _zfs_is_dataset "${POT_ZFS_ROOT}" ; then
+			echo "${POT_ZFS_ROOT} is an invalid POT root"
+			return 1 # false
+		fi
+		# create the pot root
+		echo zfs create -o mountpoint=${POT_FS_ROOT} -o canmount=off -o compression=lz4 -o atime=off ${POT_ZFS_ROOT}
+	else
+		echo "${POT_ZFS_ROOT} already present"
+	fi
 
 	# create the root directory
-	echo mkdir -p ${POT_FS_ROOT}
+	if [ ! -d ${POT_FS_ROOT} ]; then
+		echo mkdir -p ${POT_FS_ROOT}
+		if [ ! -d ${POT_FS_ROOT} ]; then
+			echo "Not able to create the dir ${POT_FS_ROOT}"
+			return 1 # false
+		fi
+	fi
 
 	# create mandatory datasets
 

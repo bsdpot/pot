e406a54a8f8ce4ef02a6b1f9263d9ebdd756233c
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-10-18 00:05:32 +0200

    Add snapshot implementation

---
+++ share/pot/snapshot.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# supported releases
+snapshot-help()
+{
+	echo "pot jstop [-h][-v][-f] [jailname]"
+	echo '  -h print this help'
+	echo '  -v verbose'
+	echo '  -f full'
+	echo '  jailname : the jail that has to start'
+}
+
+pot-snapshot()
+{
+	local _pname _full
+	args=$(getopt hvf $*)
+	_full="NO"
+	set -- $args
+	while true; do
+		case "$1" in
+		-h)
+			snapshot-help
+			exit 0
+			;;
+		-v)
+			_POT_VERBOSITY=$(( _POT_VERBOSITY + 1))
+			shift
+			;;
+		-f)
+			_full="YES"
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			snapshot-help
+			exit 1
+		esac
+	done
+	_pname=$1
+	if [ -z "$_pname" ]; then
+		_error "A jail name is mandatory"
+		snapshot-help
+		exit 1
+	fi
+	if [ "$_full" = "YES" ]; then
+		_pot_zfs_snap_full $_pname
+	else
+		_pot_zfs_snap $_pname
+	fi
+}

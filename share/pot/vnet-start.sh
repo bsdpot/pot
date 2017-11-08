7f6c113fcc07b0e861887c92c2f44b25a835d029
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-11-08 14:54:25 +0100

    Rework of vnet-start

--- share/pot/vnet-start.sh
+++ share/pot/vnet-start.sh
@@ -10,7 +10,7 @@ vnet-start-help()
 
 pot-vnet-start()
 {
-	local _bridge
+	local _bridge _pfrules
 	args=$(getopt hr:v $*)
 	if [ $? -ne 0 ]; then
 		vnet-start-help
@@ -34,6 +34,7 @@ pot-vnet-start()
 		esac
 	done
 
+	# Check configuration
 	if [ -z "${POT_NETWORK}" -o -z "${POT_GATEWAY}" ]; then
 		_error "No network or gateway defined"
 		exit 1
@@ -43,19 +44,35 @@ pot-vnet-start()
 		exit 1
 	fi
 
-	if [ -w "/tmp/pot_pfrules" ]; then
-		rm /tmp/pot_pfrules
+	# bridge creation
+	# if bridge0 doesn't exist yet
+	_bridge=$(ifconfig bridge create)
+	if [ $? -ne 0 ]; then
+		_error "Bridge not created"
+	else
+		_debug "Bridge created $_bridge"
+	fi
+	ifconfig $_bridge inet $POT_GATEWAY netmask $POT_NETMASK
+	if [ $? -ne 0 ]; then
+		_error "Error during bridge configuration ($_bridge)"
+	else
+		_debug "Bridge $_bridge configured with IP $POT_GATEWAY netmask $POT_NETMASK"
+	fi
+
+	# firewall rules
+	_pfrules="/tmp/pot_pfrules"
+	if [ -w "$_pfrules" ]; then
+		rm -f $_pfrules
 	fi
 	(
 		echo "ext_if = \"${POT_EXTIF}\""
 		echo "localnet = \"${POT_NETWORK}\""
 		echo "nat on \$ext_if from \$localnet to any -> (\$ext_if)"
-	) > /tmp/pot_pfrules
+	) > $_pfrules
 
-	pfctl -F all -f /tmp/pot_pfrules
-	## TODO if verbose, pfctl -s nat
-	# if bridge0 doesn't exist yet
-	_bridge=$(ifconfig bridge create)
-	ifconfig $_bridge inet $POT_GATEWAY netmask $POT_NETMASK
+	pfctl -F nat -f $_pfrules
+	if _is_verbose ; then
+		pfctl -s nat
+	fi
 }
 

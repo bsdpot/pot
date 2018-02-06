ed2fe04d37ed43cddbb992b943948f075fa68a95
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2018-02-07 00:43:13 +0100

    Add sh -n to validate the syntax before to execute tests

--- tests/test-suite.sh
+++ tests/test-suite.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 DOIT=
-
+SHL=sh
 if [ "$(uname)" = "Linux" ]; then
 	# using bash explicitely because of travis unkowns about /bin/sh
 	DOIT=bash
+	SHL=bash
+fi
+
+if ! $SHL -n ../bin/pot ../share/pot/*.sh ; then
+	exit 1
+else
+	echo "Syntax check passed"
+	echo
 fi
 
 suites=$(ls *.sh)

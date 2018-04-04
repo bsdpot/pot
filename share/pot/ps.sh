c88000bf0c88b312a6d712f9be4bfbb83dbf450a
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2018-04-04 16:57:59 +0200

    Add the first implementation of the command ps

---
+++ share/pot/ps.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# supported releases
+ps-help()
+{
+	echo "pot ps [-hvq]"
+	echo '  -h print this help'
+	echo '  -v verbose'
+	echo '  -q quite: print only active pot'\''s name'
+}
+
+# $1 pot name
+_ps_pot()
+{
+	local _pname _q
+	_pname=$1
+	_q=$2
+	if _is_pot_running "$_pname" ; then
+		if [ "$_q" = "quiet" ]; then
+			echo "$_pname"
+			return
+		fi
+		echo "$_pname"
+	fi
+}
+
+_ps_pots()
+{
+	local _jdir _pots _q _p
+	_q="$1"
+	_jdir="${POT_FS_ROOT}/jails/"
+	_pots=$( find "$_jdir/" -type d -mindepth 1 -maxdepth 1 -exec basename {} \; | tr '\n' ' ' )
+	for _p in $_pots; do
+		_ps_pot "$_p" "$_q"
+	done
+}
+
+pot-ps()
+{
+	local _q
+	_q=
+	if ! args=$(getopt hvq "$@") ; then
+		ps-help
+		${EXIT} 1
+	fi
+	set -- $args
+	while true; do
+		case "$1" in
+		-h)
+			ps-help
+			${EXIT} 0
+			;;
+		-v)
+			_POT_VERBOSITY=$(( _POT_VERBOSITY + 1))
+			shift
+			;;
+		-q)
+			_q="quiet"
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		esac
+	done
+	_ps_pots "$_q"
+}

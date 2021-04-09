4fc0df8f7c0dac92de4b6976a8158afc1bc64c50
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-04-09 23:46:25 +0200

    copy-in: rework the feature to be executed inside the jail
    Perform the copy from the host can have devastating effects if
    in the pot the destination is using symbolic links with absolute
    paths.
    This implementation is the outcome of a fruitful discussion with
    jmg@ about
    `/bin/cp` needs to be present in the jail, to perform the copy

--- share/pot/copy-in.sh
+++ share/pot/copy-in.sh
@@ -7,6 +7,7 @@ copy-in-help()
 	echo "pot copy-in [-hv] -p pot -s source -d destination"
 	echo '  -h print this help'
 	echo '  -v verbose'
+	echo '  -F force copy operation for running jails (can partially expose the host file system)'
 	echo '  -p pot : the working pot'
 	echo '  -s source : the file to be added component to be added'
 	echo '  -d destination : the final location inside the pot'
@@ -16,7 +17,7 @@ copy-in-help()
 _source_validation()
 {
 	# shellcheck disable=SC2039
-	local _pname _source _destination
+	local _source
 	_source="$1"
 	if [ -f "$_source" ] || [ -d "$_source" ]; then
 		if [ -r "$_source" ]; then
@@ -30,21 +31,48 @@ _source_validation()
 	fi
 }
 
+_mount_source_into_potroot()
+{
+	# shellcheck disable=SC2039
+	local _source _proot _source_mnt
+	_source="$1"
+	_proot="$2"
+	if [ -f "$_source" ]; then
+		_source_mnt="$( dirname "$_source" )"
+	else
+		_source_mnt="$_source"
+	fi
+
+	if ! mkdir "$_proot/tmp/tmp" ; then
+		_error "Failed to created the temporary mount point inside the pot"
+		return 1
+	fi
+	if ! mount_nullfs -o ro "$_source_mnt" "$_proot/tmp/tmp" ; then
+		_error "Failed to mount source inside the pot"
+	fi
+}
+
 # shellcheck disable=SC2039
 pot-copy-in()
 {
-	local _pname _source _destination _to_be_umount _rc
+	local _pname _source _destination _to_be_umount _rc _force _proot _cp_opt
 	OPTIND=1
 	_pname=
 	_destination=
-	while getopts "hvs:d:p:" _o ; do
+	_force=
+	_cp_opt="-a"
+	while getopts "hvs:d:p:F" _o ; do
 		case "$_o" in
 		h)
 			copy-in-help
 			return 0
 			;;
+		F)
+			_force="YES"
+			;;
 		v)
 			_POT_VERBOSITY=$(( _POT_VERBOSITY + 1))
+			_cp_opt="-va"
 			;;
 		s)
 			_source="$OPTARG"
@@ -95,20 +123,50 @@ pot-copy-in()
 		copy-in-help
 		return 1
 	fi
-	if ! _is_pot_running "$_pname" ; then 
+	if _is_pot_running "$_pname" ; then
+		if [ "$_force" != "YES" ]; then
+			_error "Copying files on a running pot is discouraged, it can partially expose the host file system to the jail"
+			_info "Using the -F flag, the operation can be executed anyway, but we disagree"
+			return 1
+		else
+			_debug "Copying files on a running pot allowed, because of the -F flag"
+		fi
+	else
 		_pot_mount "$_pname"
 		_to_be_umount=1
 	fi
-	if _is_verbose ; then
-		_cp_opt="-va"
-	else
-		_cp_opt="-a"
+	_proot=${POT_FS_ROOT}/jails/$_pname/m
+	if ! _mount_source_into_potroot "$_source" "$_proot" ; then
+		if [ "$_to_be_umount" = "1" ]; then
+			_pot_umount "$_pname"
+		fi
+		return 1
 	fi
-	if cp "$_cp_opt" "$_source" "${POT_FS_ROOT}/jails/$_pname/m/$_destination" ; then
-		_debug "Source $_source copied in the pot $_pname"
-		_rc=0
+	if [ -f "$_source" ]; then
+		_cp_source="/tmp/tmp/$( basename "$_source" )"
 	else
-		_error "Source $_source NOT copied because of an error"
+		_cp_source=/tmp/tmp
+	fi
+	if _is_pot_running "$_pname" ; then
+		if jexec "$_pname" /bin/cp "$_cp_opt" "/tmp/tmp/$_cp_source" "$_destination" ; then
+			_debug "Source $_source copied in the pot $_pname"
+			_rc=0
+		else
+			_error "Source $_source NOT copied because of an error"
+			_rc=1
+		fi
+	else
+		if jail -c path="$_proot" command=/bin/cp "$_cp_opt" "/tmp/tmp/$_cp_source" "$_destination" ; then
+			_debug "Source $_source copied in the pot $_pname"
+			_rc=0
+		else
+			_error "Source $_source NOT copied because of an error"
+			_rc=1
+		fi
+	fi
+
+	if umount "$_proot/tmp/tmp" ; then
+		_error "Failed to unmount the source tmp folder from the pot"
 		_rc=1
 	fi
 	if [ "$_to_be_umount" = "1" ]; then

c89a171b9438de4e1afbb842e3827ed5e3c3d719
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-04-18 22:28:43 +0200

    Make copy-in more robust (#144)
    * copy-in: rework the feature to be executed inside the jail
    
    Perform the copy from the host can have devastating effects if in the pot the destination is using symbolic links with absolute paths.
    This implementation is the outcome of a fruitful discussion with jmg@ about it.
    `/bin/cp` needs to be present in the jail, to perform the copy
    
    * copy-in: adapt test
    While testing, found and fixed two bugs
    
    * copy-in: make it mutually exclusive with other create/clone commands
    
    * CI: add copy-in test

--- tests/copy-in1.sh
+++ tests/copy-in1.sh
@@ -2,8 +2,33 @@
 
 # system utilities stubs
 
-cp() {
-	__monitor CP "$@"
+if [ "$(uname)" = "Linux" ]; then
+	TEST=/usr/bin/[
+else
+	TEST=/bin/[
+fi
+
+[()
+{
+	if ${TEST} "$1" = "-f" ]; then
+		if ${TEST} "$2" = "test-file" ]; then
+			return 0 # false
+		fi
+	fi
+	${TEST} "$@"
+	return $?
+}
+
+jexec() {
+	__monitor JEXEC "$@"
+}
+
+jail() {
+	__monitor JAIL "$@"
+}
+
+umount() {
+	__monitor UMOUNT "$@"
 }
 
 # UUT
@@ -28,6 +53,11 @@ _source_validation()
 		return 0 # true
 	fi
 	return 1 # false
+}
+
+_mount_source_into_potroot()
+{
+	return 0 # true
 }
 
 _pot_mount()
@@ -144,6 +174,19 @@ test_pot_copy_in_005()
 	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
 }
 
+test_pot_copy_in_006()
+{
+	pot-copy-in -p test-pot-run -s test-file -d /test-mnt
+	assertEquals "Exit rc" "1" "$?"
+	assertEquals "Help calls" "0" "$HELP_CALLS"
+	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
+	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
+	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
+	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
+	assertEquals "_jail calls" "0" "$JAIL_CALLS"
+}
+
 test_pot_copy_in_020()
 {
 	pot-copy-in -p test-pot -s test-file -d /test-mnt
@@ -154,16 +197,17 @@ test_pot_copy_in_020()
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
 	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
 	assertEquals "_pot_mount calls" "1" "$PMOUNT_CALLS"
-	assertEquals "_cp calls" "1" "$CP_CALLS"
-	assertEquals "_cp args" "-a" "$CP_CALL1_ARG1"
-	assertEquals "_cp args" "test-file" "$CP_CALL1_ARG2"
-	assertEquals "_cp args" "/tmp/jails/test-pot/m/test-mnt" "$CP_CALL1_ARG3"
+	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
+	assertEquals "_jail calls" "1" "$JAIL_CALLS"
+	assertEquals "_jail args" "-c" "$JAIL_CALL1_ARG1"
+	assertEquals "_jail args" "/tmp/tmp/test-file" "$JAIL_CALL1_ARG5"
+	assertEquals "_jail args" "/test-mnt" "$JAIL_CALL1_ARG6"
 	assertEquals "_pot_umount calls" "1" "$PUMOUNT_CALLS"
 }
 
 test_pot_copy_in_021()
 {
-	pot-copy-in -p test-pot-run -s test-file -d /test-mnt
+	pot-copy-in -p test-pot-run -s test-file -d /test-mnt -F
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
@@ -171,16 +215,18 @@ test_pot_copy_in_021()
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
 	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
 	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
-	assertEquals "_cp calls" "1" "$CP_CALLS"
-	assertEquals "_cp args" "-a" "$CP_CALL1_ARG1"
-	assertEquals "_cp args" "test-file" "$CP_CALL1_ARG2"
-	assertEquals "_cp args" "/tmp/jails/test-pot-run/m/test-mnt" "$CP_CALL1_ARG3"
+	assertEquals "_jexec calls" "1" "$JEXEC_CALLS"
+	assertEquals "_jexec args" "test-pot-run" "$JEXEC_CALL1_ARG1"
+	assertEquals "_jexec args" "-a" "$JEXEC_CALL1_ARG3"
+	assertEquals "_jexec args" "/tmp/tmp/test-file" "$JEXEC_CALL1_ARG4"
+	assertEquals "_jexec args" "/test-mnt" "$JEXEC_CALL1_ARG5"
+	assertEquals "_jail calls" "0" "$JAIL_CALLS"
 	assertEquals "_pot_umount calls" "0" "$PUMOUNT_CALLS"
 }
 
 test_pot_copy_in_022()
 {
-	pot-copy-in -p test-pot-run -s test-file -d /test-mnt -v
+	pot-copy-in -p test-pot-run -s test-file -d /test-mnt -vF
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
@@ -188,10 +234,12 @@ test_pot_copy_in_022()
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
 	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
 	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
-	assertEquals "_cp calls" "1" "$CP_CALLS"
-	assertEquals "_cp args" "-va" "$CP_CALL1_ARG1"
-	assertEquals "_cp args" "test-file" "$CP_CALL1_ARG2"
-	assertEquals "_cp args" "/tmp/jails/test-pot-run/m/test-mnt" "$CP_CALL1_ARG3"
+	assertEquals "_jexec calls" "1" "$JEXEC_CALLS"
+	assertEquals "_jexec args" "test-pot-run" "$JEXEC_CALL1_ARG1"
+	assertEquals "_jexec args" "-va" "$JEXEC_CALL1_ARG3"
+	assertEquals "_jexec args" "/tmp/tmp/test-file" "$JEXEC_CALL1_ARG4"
+	assertEquals "_jexec args" "/test-mnt" "$JEXEC_CALL1_ARG5"
+	assertEquals "_jail calls" "0" "$JAIL_CALLS"
 	assertEquals "_pot_umount calls" "0" "$PUMOUNT_CALLS"
 }
 
@@ -205,19 +253,24 @@ test_pot_copy_in_023()
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
 	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
 	assertEquals "_pot_mount calls" "1" "$PMOUNT_CALLS"
-	assertEquals "_cp calls" "1" "$CP_CALLS"
-	assertEquals "_cp args" "-va" "$CP_CALL1_ARG1"
-	assertEquals "_cp args" "test-dir" "$CP_CALL1_ARG2"
-	assertEquals "_cp args" "/tmp/jails/test-pot/m/test-mnt" "$CP_CALL1_ARG3"
+	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
+	assertEquals "_jail calls" "1" "$JAIL_CALLS"
+	assertEquals "_jail args" "-c" "$JAIL_CALL1_ARG1"
+	assertEquals "_jail args" "/tmp/tmp" "$JAIL_CALL1_ARG5"
+	assertEquals "_jail args" "/test-mnt" "$JAIL_CALL1_ARG6"
 	assertEquals "_pot_umount calls" "1" "$PUMOUNT_CALLS"
 }
 
 setUp()
 {
 	common_setUp
+	ERROR_DEBUG="NO"
+	DEBUG_DEBUG="NO"
 	HELP_CALLS=0
 	SRCVALID_CALLS=0
-	CP_CALLS=0
+	JAIL_CALLS=0
+	JEXEC_CALLS=0
+	UMOUNT_CALLS=0
 	PMOUNT_CALLS=0
 	PUMOUNT_CALLS=0
 

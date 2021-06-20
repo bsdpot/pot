0d619c640304c210f363083ae463300343939492
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-06-20 18:05:31 +0200

    create-base: fix help message
    Fix help message suggesting the currently supported FreeBSD
    releases

--- share/pot/create-base.sh
+++ share/pot/create-base.sh
@@ -9,7 +9,7 @@ create-base-help()
 	echo "pot create-base [-h] [-r RELEASE] [-b basename]"
 	echo '  -h print this help'
 	echo '  -v verbose'
-	echo '  -r RELEASE : supported release are:'"${_POT_RELEASES}"
+	echo '  -r RELEASE : supported release are:'"$( _get_valid_releases )"
 	echo '  -b base name : optional, (default: the release)'
 }
 

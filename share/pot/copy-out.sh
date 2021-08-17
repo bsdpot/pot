fd26671789ea2adea15d34a1fa8e1964f7a1a43f
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-08-17 12:05:29 +0200

    Copy out (#165)
    * copy-out: initial implementation with tests
    * copy-out: add it to pot
    * copy-in: adopt mktemp to create the mountpoint
    * copy-[in,out]: remove the temp dir if needed
    * zsh: add copy-out
    
    Fixes #162

--- share/pot/copy-out.sh
+++ share/pot/copy-out.sh
@@ -2,65 +2,55 @@
 :
 
 # shellcheck disable=SC3033
-copy-in-help()
+copy-out-help()
 {
-	echo "pot copy-in [-hv] -p pot -s source -d destination"
+	echo "pot copy-out [-hv] -p pot -s source -d destination"
 	echo '  -h print this help'
 	echo '  -v verbose'
 	echo '  -F force copy operation for running jails (can partially expose the host file system)'
 	echo '  -p pot : the working pot'
-	echo '  -s source : the file or directory to be copied in'
-	echo '  -d destination : the final location inside the pot'
+	echo '  -s source : the file or directory inside the pot'
+	echo '  -d destination : the location (directory) outside the pot to copy the source'
 }
 
 # $1 source
-_source_validation()
+_destination_validation()
 {
 	# shellcheck disable=SC3043
-	local _source
-	_source="$1"
-	if [ -f "$_source" ] || [ -d "$_source" ]; then
-		if [ -r "$_source" ]; then
-			return 0 # true
-		else
-			_error "$_source not readable"
-		fi
+	local _destination
+	_destination="$1"
+	if [ -r "$_destination" ] && [ -d "$_destination" ] && [ -x "$_destination" ]; then
+		return 0 # true
 	else
-		_error "$_source not valid"
-		return 1 # false
+		_error "$_destination not valid"
 	fi
 }
 
-_make_temp_source()
+_make_temp_destination()
 {
 	# shellcheck disable=SC3043
 	local _proot
-	_proot="$2"
-	mktemp -d "$_proot/tmp/copy-in${POT_MKTEMP_SUFFIX}"
+	_proot="$1"
+	mktemp -d "$_proot/tmp/copy-out${POT_MKTEMP_SUFFIX}"
 }
 
-_mount_source_into_potroot()
+_mount_destination_into_potroot()
 {
 	# shellcheck disable=SC3043
-	local _source _mountpoint _source_mnt
-	_source="$1"
+	local _destination _mountpoint
+	_destination="$1"
 	_mountpoint="$2"
-	if [ -f "$_source" ]; then
-		_source_mnt="$( dirname "$_source" )"
-	else
-		_source_mnt="$_source"
-	fi
-	if ! mount_nullfs -o ro "$_source_mnt" "$_mountpoint" ; then
-		_error "Failed to mount source inside the pot"
+	if ! mount_nullfs "$_destination" "$_mountpoint" ; then
+		_error "Failed to mount destination inside the pot"
 		return 1
 	fi
 }
 
 # shellcheck disable=SC3033
-pot-copy-in()
+pot-copy-out()
 {
 	# shellcheck disable=SC3043
-	local _pname _source _destination _to_be_umount _rc _force _proot _cp_opt
+	local _pname _source _destination _to_be_umount _rc _force _proot _cp_opt _destination_mountpoint
 	OPTIND=1
 	_pname=
 	_destination=
@@ -69,7 +59,7 @@ pot-copy-in()
 	while getopts "hvs:d:p:F" _o ; do
 		case "$_o" in
 		h)
-			copy-in-help
+			copy-out-help
 			return 0
 			;;
 		F)
@@ -89,7 +79,7 @@ pot-copy-in()
 			_destination="$OPTARG"
 			;;
 		*)
-			copy-in-help
+			copy-out-help
 			return 1
 			;;
 		esac
@@ -97,78 +87,74 @@ pot-copy-in()
 
 	if [ -z "$_pname" ]; then
 		_error "A pot name is mandatory"
-		copy-in-help
+		copy-out-help
 		return 1
 	fi
 	if [ -z "$_source" ]; then
 		_error "A source is mandatory"
-		copy-in-help
+		copy-out-help
 		return 1
 	fi
 	if [ -z "$_destination" ]; then
 		_error "A destination is mandatory"
-		copy-in-help
+		copy-out-help
 		return 1
 	fi
-	if ! _is_absolute_path "$_destination" ; then
-		_error "The destination has to be an absolute pathname"
+	if ! _is_absolute_path "$_source" ; then
+		_error "The source has to be an absolute pathname"
 		return 1
 	fi
 
 	if ! _is_pot "$_pname" ; then
 		_error "pot $_pname is not valid"
-		copy-in-help
+		copy-out-help
 		return 1
 	fi
 	if ! _is_uid0 ; then
 		return 1
 	fi
-	if ! _source_validation "$_source" ; then
-		copy-in-help
+	if ! _destination_validation "$_destination" ; then
+		copy-out-help
 		return 1
 	fi
 	if _is_pot_running "$_pname" ; then
 		if [ "$_force" != "YES" ]; then
-			_error "Copying files on a running pot is discouraged, it can partially expose the host file system to the jail"
+			_error "Copying files from a running pot is discouraged, it can partially expose the host file system to the jail"
 			_info "Using the -F flag, the operation can be executed anyway, but we disagree"
 			return 1
 		else
-			_debug "Copying files on a running pot allowed, because of the -F flag"
+			_debug "Copying files from a running pot allowed, because of the -F flag"
 		fi
 	else
 		_pot_mount "$_pname"
 		_to_be_umount=1
 	fi
 	_proot=${POT_FS_ROOT}/jails/$_pname/m
-	if ! _source_mountpoint="$( _make_temp_source "$_proot" )" ; then
+	if ! _destination_mountpoint="$( _make_temp_destination "$_proot" )" ; then
 		_error "Failed to build a temporary folder in the pot /tmp"
 		if [ "$_to_be_umount" = "1" ]; then
 			_pot_umount "$_pname"
 		fi
 		return 1
 	fi
-	if ! _mount_source_into_potroot "$_source" "$_source_mountpoint" ; then
+	if ! _mount_destination_into_potroot "$_destination" "$_destination_mountpoint" ; then
 		if [ "$_to_be_umount" = "1" ]; then
 			_pot_umount "$_pname"
 		fi
 		return 1
 	fi
-	if [ -f "$_source" ]; then
-		_cp_source="/tmp/$( basename "$_source_mountpoint" )/$( basename "$_source" )"
-	else
-		_cp_source="/tmp/$( basename "$_source_mountpoint" )"
-	fi
+	_cp_destination="/tmp/$( basename "$_destination_mountpoint" )"
 	if _is_pot_running "$_pname" ; then
-		if jexec "$_pname" /bin/cp "$_cp_opt" "$_cp_source" "$_destination" ; then
-			_debug "Source $_source copied in the pot $_pname"
+		if jexec "$_pname" /bin/cp "$_cp_opt" "$_source" "$_cp_destination" ; then
+			_debug "Source $_source copied from the pot $_pname"
 			_rc=0
 		else
 			_error "Source $_source NOT copied because of an error"
 			_rc=1
 		fi
 	else
-		if jail -c path="$_proot" command=/bin/cp "$_cp_opt" "$_cp_source" "$_destination" ; then
-			_debug "Source $_source copied in the pot $_pname"
+		if jail -c path="$_proot" command=/bin/cp "$_cp_opt" "$_source" "$_cp_destination" ; then
+			_debug "Source $_source copied from the pot $_pname"
 			_rc=0
 		else
 			_error "Source $_source NOT copied because of an error"
@@ -176,14 +162,14 @@ pot-copy-in()
 		fi
 	fi
 
-	if ! umount -f "$_source_mountpoint" ; then
+	if ! umount -f "$_destination_mountpoint" ; then
 		_error "Failed to unmount the source tmp folder from the pot"
 		_rc=1
 	fi
 	if [ "$_to_be_umount" = "1" ]; then
 		_pot_umount "$_pname"
 	else
-		rmdir "$_source_mountpoint"
+		rmdir "$_destination_mountpoint"
 	fi
 	return $_rc
 }
--- tests/copy-out1.sh
+++ tests/copy-out1.sh
@@ -29,42 +29,40 @@ jail() {
 
 mktemp() {
 	__monitor MKTEMP "$@"
-	echo /tmp/copy-in.asdf
-}
-
-umount() {
-	__monitor UMOUNT "$@"
+	echo /tmp/copy-out.asdf
 }
 
 rmdir() {
 	__monitor RMDIR "$@"
 }
 
+umount() {
+	__monitor UMOUNT "$@"
+}
+
 # UUT
-. ../share/pot/copy-in.sh
+. ../share/pot/copy-out.sh
 
 # common stubs
 . common-stub.sh
 
 # app specific stubs
-copy-in-help()
+copy-out-help()
 {
 	__monitor HELP "$@"
 	return 0 # true
 }
 
-_source_validation()
+_destination_validation()
 {
-	__monitor SRCVALID "$@"
-	if [ "$1" = "test-file" ]; then
-		return 0 # true
-	elif [ "$1" = "test-dir" ]; then
+	__monitor DSTVALID "$@"
+	if [ "$1" = "test-mnt" ]; then
 		return 0 # true
 	fi
 	return 1 # false
 }
 
-_mount_source_into_potroot()
+_mount_destination_into_potroot()
 {
 	return 0 # true
 }
@@ -79,9 +77,9 @@ _pot_umount()
 	__monitor PUMOUNT "$@"
 }
 
-test_pot_copy_in_001()
+test_pot_copy_out_001()
 {
-	pot-copy-in
+	pot-copy-out
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
@@ -89,7 +87,7 @@ test_pot_copy_in_001()
 	assertEquals "_is_uid0 calls" "0" "$ISUID0_CALLS"
 
 	setUp
-	pot-copy-in -vb
+	pot-copy-out -vb
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
@@ -97,75 +95,75 @@ test_pot_copy_in_001()
 	assertEquals "_is_uid0 calls" "0" "$ISUID0_CALLS"
 
 	setUp
-	pot-copy-in -b bb
+	pot-copy-out -b bb
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "0" "$ISUID0_CALLS"
 
 	setUp
-	pot-copy-in -h
+	pot-copy-out -h
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 }
 
-test_pot_copy_in_002()
+test_pot_copy_out_002()
 {
-	pot-copy-in -p test-pot
+	pot-copy-out -p test-pot
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 
 	setUp
-	pot-copy-in -s test-file
+	pot-copy-out -s test-file
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 
 	setUp
-	pot-copy-in -d test-mnt
+	pot-copy-out -d test-mnt
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 
 	setUp
-	pot-copy-in -p test-pot -s test-file
+	pot-copy-out -p test-pot -s test-file
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 
 	setUp
-	pot-copy-in -d test-mnt -s test-file
+	pot-copy-out -d test-mnt -s test-file
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 
 	setUp
-	pot-copy-in -d test-mnt -p test-pot
+	pot-copy-out -d test-mnt -p test-pot
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "0" "$ISPOT_CALLS"
 }
 
-test_pot_copy_in_003()
+test_pot_copy_out_003()
 {
-	pot-copy-in -p test-no-pot -s test-no-file -d /test-no-mnt
+	pot-copy-out -p test-no-pot -s /test-no-file -d test-no-mnt
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 }
 
-test_pot_copy_in_004()
+test_pot_copy_out_004()
 {
-	pot-copy-in -p test-no-pot -s test-file -d /test-no-mnt
+	pot-copy-out -p test-no-pot -s /test-file -d test-no-mnt
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"
@@ -173,103 +171,103 @@ test_pot_copy_in_004()
 	assertEquals "_is_uid0 calls" "0" "$ISUID0_CALLS"
 }
 
-test_pot_copy_in_005()
+test_pot_copy_out_005()
 {
-	pot-copy-in -p test-pot -s test-no-file -d /test-no-mnt
+	pot-copy-out -p test-pot -s /test-no-file -d test-no-mnt
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 }
 
-test_pot_copy_in_006()
+test_pot_copy_out_006()
 {
-	pot-copy-in -p test-pot-run -s test-file -d /test-mnt
+	pot-copy-out -p test-pot-run -s /test-file -d test-mnt
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
 	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
 	assertEquals "_jail calls" "0" "$JAIL_CALLS"
 }
 
-test_pot_copy_in_020()
+test_pot_copy_out_020()
 {
-	pot-copy-in -p test-pot -s test-file -d /test-mnt
+	pot-copy-out -p test-pot -s /test-file -d test-mnt
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 	assertEquals "_pot_mount calls" "1" "$PMOUNT_CALLS"
 	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
 	assertEquals "_jail calls" "1" "$JAIL_CALLS"
 	assertEquals "_jail args" "-c" "$JAIL_CALL1_ARG1"
-	assertEquals "_jail args" "/tmp/copy-in.asdf/test-file" "$JAIL_CALL1_ARG5"
-	assertEquals "_jail args" "/test-mnt" "$JAIL_CALL1_ARG6"
+	assertEquals "_jail args" "/test-file" "$JAIL_CALL1_ARG5"
+	assertEquals "_jail args" "/tmp/copy-out.asdf" "$JAIL_CALL1_ARG6"
 	assertEquals "_pot_umount calls" "1" "$PUMOUNT_CALLS"
 	assertEquals "_rmdir calls" "0" "$RMDIR_CALLS"
 }
 
-test_pot_copy_in_021()
+test_pot_copy_out_021()
 {
-	pot-copy-in -p test-pot-run -s test-file -d /test-mnt -F
+	pot-copy-out -p test-pot-run -s /test-file -d test-mnt -F
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
 	assertEquals "_jexec calls" "1" "$JEXEC_CALLS"
 	assertEquals "_jexec args" "test-pot-run" "$JEXEC_CALL1_ARG1"
 	assertEquals "_jexec args" "-a" "$JEXEC_CALL1_ARG3"
-	assertEquals "_jexec args" "/tmp/copy-in.asdf/test-file" "$JEXEC_CALL1_ARG4"
-	assertEquals "_jexec args" "/test-mnt" "$JEXEC_CALL1_ARG5"
+	assertEquals "_jexec args" "/test-file" "$JEXEC_CALL1_ARG4"
+	assertEquals "_jexec args" "/tmp/copy-out.asdf" "$JEXEC_CALL1_ARG5"
 	assertEquals "_jail calls" "0" "$JAIL_CALLS"
 	assertEquals "_pot_umount calls" "0" "$PUMOUNT_CALLS"
 	assertEquals "_rmdir calls" "1" "$RMDIR_CALLS"
 }
 
-test_pot_copy_in_022()
+test_pot_copy_out_022()
 {
-	pot-copy-in -p test-pot-run -s test-file -d /test-mnt -vF
+	pot-copy-out -p test-pot-run -s /test-file -d test-mnt -vF
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 	assertEquals "_pot_mount calls" "0" "$PMOUNT_CALLS"
 	assertEquals "_jexec calls" "1" "$JEXEC_CALLS"
 	assertEquals "_jexec args" "test-pot-run" "$JEXEC_CALL1_ARG1"
 	assertEquals "_jexec args" "-va" "$JEXEC_CALL1_ARG3"
-	assertEquals "_jexec args" "/tmp/copy-in.asdf/test-file" "$JEXEC_CALL1_ARG4"
-	assertEquals "_jexec args" "/test-mnt" "$JEXEC_CALL1_ARG5"
+	assertEquals "_jexec args" "/test-file" "$JEXEC_CALL1_ARG4"
+	assertEquals "_jexec args" "/tmp/copy-out.asdf" "$JEXEC_CALL1_ARG5"
 	assertEquals "_jail calls" "0" "$JAIL_CALLS"
 	assertEquals "_pot_umount calls" "0" "$PUMOUNT_CALLS"
 	assertEquals "_rmdir calls" "1" "$RMDIR_CALLS"
 }
 
-test_pot_copy_in_023()
+test_pot_copy_out_023()
 {
-	pot-copy-in -p test-pot -s test-dir -d /test-mnt -v
+	pot-copy-out -p test-pot -s /test-dir -d test-mnt -v
 	assertEquals "Exit rc" "0" "$?"
 	assertEquals "Help calls" "0" "$HELP_CALLS"
 	assertEquals "Error calls" "0" "$ERROR_CALLS"
 	assertEquals "_is_pot calls" "1" "$ISPOT_CALLS"
 	assertEquals "_is_uid0 calls" "1" "$ISUID0_CALLS"
-	assertEquals "_source_validation calls" "1" "$SRCVALID_CALLS"
+	assertEquals "_destination_validation calls" "1" "$DSTVALID_CALLS"
 	assertEquals "_pot_mount calls" "1" "$PMOUNT_CALLS"
 	assertEquals "_jexec calls" "0" "$JEXEC_CALLS"
 	assertEquals "_jail calls" "1" "$JAIL_CALLS"
 	assertEquals "_jail args" "-c" "$JAIL_CALL1_ARG1"
-	assertEquals "_jail args" "/tmp/copy-in.asdf" "$JAIL_CALL1_ARG5"
-	assertEquals "_jail args" "/test-mnt" "$JAIL_CALL1_ARG6"
+	assertEquals "_jail args" "/test-dir" "$JAIL_CALL1_ARG5"
+	assertEquals "_jail args" "/tmp/copy-out.asdf" "$JAIL_CALL1_ARG6"
 	assertEquals "_pot_umount calls" "1" "$PUMOUNT_CALLS"
 	assertEquals "_rmdir calls" "0" "$RMDIR_CALLS"
 }
@@ -280,14 +278,14 @@ setUp()
 	ERROR_DEBUG="NO"
 	DEBUG_DEBUG="NO"
 	HELP_CALLS=0
-	SRCVALID_CALLS=0
+	DSTVALID_CALLS=0
 	JAIL_CALLS=0
 	JEXEC_CALLS=0
 	MKTEMP_CALLS=0
+	RMDIR_CALLS=0
 	UMOUNT_CALLS=0
 	PMOUNT_CALLS=0
 	PUMOUNT_CALLS=0
-	RMDIR_CALLS=0
 
 	POT_FS_ROOT=/tmp
 	POT_ZFS_ROOT=zpot

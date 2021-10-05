bsdpot/pot

commit 8967a2e702a26f8d9bb4e63d0fe92ee1daa79c85

prepare: change network type while preparing
    prepare: add support to -N to change network type while preparing
    clone: with public-bridge, -i has auto as default value
    execute: remove the alias, it's wrong and better remove it now

share/pot/prepare.sh
@@ -4,7 +4,8 @@
 # shellcheck disable=SC2039
 prepare-help()
 {
-	echo "pot prepare [-hvS] -p pot -U URL -t tag -a aID -n potname -c cmd [-e port]"
+	echo "pot prepare [-hvS] -p pot -U URL -t tag -a aID -n potname -c cmd"
+	echo '        [-e port] [-N network-type] [-i ipaddr]'
 	echo '  -h print this help'
 	echo '  -h verbose'
 	echo '  -p pot : the pot image'
@@ -13,6 +14,8 @@ prepare-help()
 	echo '  -a aID : the allocation ID'
 	echo '  -n potname : the new potname (used instead of pot_tag)'
 	echo '  -c cmd : the command line to start the container'
+	echo '  -N network-type : new network type of the imported pot'
+	echo '  -i ipaddr : an ip address or the keyword auto (if applicable)'
 	echo '  -e port : the tcp port'
 	echo '            This option can be repeated multiple time, to export more ports'
 	echo '  -S : start immediately the newly generated pot'
@@ -21,12 +24,14 @@ prepare-help()
 pot-prepare()
 {
 	# shellcheck disable=SC2039
-	local _pname _o _URL _tag _tpname _cmd _ports _allocation_tag _new_pname _auto_start
+	local _pname _o _URL _tag _tpname _cmd _ports _allocation_tag _new_pname _auto_start _network_type _ipaddr
 	_pname=
 	_ports=
+	_network_type=
+	_ipaddr=
 	_auto_start="NO"
 	OPTIND=1
-	while getopts "hvp:U:t:c:e:a:n:S" _o ; do
+	while getopts "hvp:U:t:c:e:a:n:SN:i:" _o ; do
 		case "$_o" in
 		h)
 			prepare-help
@@ -67,6 +72,17 @@ pot-prepare()
 			;;
 		S)
 			_auto_start="YES"
+			;;
+		N)
+			if ! _is_in_list "$OPTARG" $_POT_NETWORK_TYPES ; then
+				_error "Network type $OPTARG not recognized"
+				clone-help
+				${EXIT} 1
+			fi
+			_network_type="$OPTARG"
+			;;
+		i)
+			_ipaddr=$OPTARG
 			;;
 		*)
 			prepare-help
@@ -115,7 +131,14 @@ pot-prepare()
 	else
 		_debug "pot $_imported_pname already imported - reusing it"
 	fi
-	if ! pot-cmd clone -P "${_imported_pname}" -p "${_new_pname}" -i auto ; then
+	_clone_network_opt=
+	if [ -n "$_network_type" ]; then
+		_clone_network_opt="-N $_network_type"
+	fi
+	if [ -n "$_ipaddr" ]; then
+		_clone_network_opt="$_clone_network_opt -i $_ipaddr"
+	fi
+	if ! pot-cmd clone -P "${_imported_pname}" -p "${_new_pname}" $_clone_network_opt ; then
 		_error "Not able to clone imported pot as $_new_pname"
 	fi
 	if [ -n "$_cmd" ]; then

bin/pot
@@ -107,7 +107,7 @@ Commands:
 	purge-snapshots -- Remove old/all snapshots
 	export -- Export a pot to a file
 	import -- Import a pot from a file or a URL
-	prepare/execute -- Import and prepare a pot - designed for jail orchestrator
+	prepare -- Import and prepare a pot - designed for jail orchestrator
 	update-config -- Update the configuration of a pot
 EOF
 }
@@ -138,9 +138,6 @@ case "${CMD}" in
 		;;
 	get-attr)
 		CMD=get-attribute
-		;;
-	execute)
-		CMD=prepare
 		;;
 esac
 

share/pot/clone.sh
@@ -287,6 +287,9 @@ pot-clone()
 	if [ "$_network_type" = "public-bridge" ] && [ "$_ipaddr" = "auto" ] ; then
 		_ipaddr="$(potnet next)"
 		_debug "-i auto: assigned $_ipaddr"
+	elif [ "$_network_type" = "public-bridge" ] && [ -z "$_ipaddr" ] ; then
+		 _ipaddr="$(potnet next)"
+		 _debug "automatically assigning $_ipaddr"
 	elif [ "$_network_type" != "public-bridge" ] && [ "$_ipaddr" = "auto" ] ; then
 		_error "Keyword auto not compatible with network type $_network_type"
 		${EXIT} 1
@@ -311,11 +314,6 @@ pot-clone()
 		fi
 		;;
 	"public-bridge")
-		if [ -z "$_ipaddr" ]; then
-			_error "$_ipaddr is mandator for alias network type"
-			clone-help
-			${EXIT} 1
-		fi
 		if ! potnet validate -H "$_ipaddr" ; then
 			_error "$_ipaddr is not a valid IP address for the public bridge"
 			clone-help

share/zsh/site-functions/_pot
@@ -263,7 +263,7 @@ _pot() {
 						'-t[tag name]::_normal' \
 						'-U[URL]:URL:_normal'
 					;;
-				prepare|execute)
+				prepare)
 					_arguments \
 						'-h[Show help]' \
 						'-v[Verbose output]' \
@@ -273,6 +273,8 @@ _pot() {
 						'-a[allocation id]::_normal' \
 						'-n[the new pot name]::_normal' \
 						'-U[URL]:URL:_normal' \
+						'-N[network type]:network type:(inherit alias public-bridge)' \
+						'-i[network config]::_normal' \
 						'*-e[port to be exported]::_normal' \
 						'-c[command]::_normal'
 					;;
@@ -348,7 +350,6 @@ _pot_cmds() {
 	'export:Export a single-type pot'
 	'import:Import a single-type pot'
 	'prepare:Import and prepare a pot - designed for jail orchestrator'
-	'execute:Import and prepare a pot - designed for jail orchestrator'
 	'update-config:Update the configuration of a pot'
 	)
 	_describe 'command' commands

tests/clone1.sh
@@ -136,8 +136,8 @@ test_pot_clone_003()
 
 test_pot_clone_004()
 {
-	# missing -i parameter
-	pot-clone -p new-pot -P test-pot-2
+	# missing -i parameter when needed
+	pot-clone -p new-pot -P test-pot-3
 	assertEquals "Exit rc" "1" "$?"
 	assertEquals "Help calls" "1" "$HELP_CALLS"
 	assertEquals "Error calls" "1" "$ERROR_CALLS"

commit 6b075d5579651cc8beeaea7fc570c140f4764566

Fix nomad host network mode
The nomad-pot-driver allows specifying `network_mode = "host"` in a
job's config stanza. This maps to pot's internal mode "inherit" and
was broken since 8967a2e7.

share/pot/prepare.sh
@@ -86,9 +86,9 @@ pot-prepare()
 				_network_type="$OPTARG"
 			fi
 			# shellcheck disable=SC2086
-			if ! _is_in_list "$OPTARG" $_POT_NETWORK_TYPES ; then
-				_error "Network type $OPTARG not recognized"
-				clone-help
+			if ! _is_in_list "$_network_type" $_POT_NETWORK_TYPES ; then
+				_error "Network type $_network_type not recognized"
+				prepare-help
 				${EXIT} 1
 			fi
 			;;